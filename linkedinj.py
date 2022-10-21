f071d37d9fd734f5a2faba2b634eee99fb1a0dda
Ayan <59755186+ayanatherate@users.noreply.github.com>
2022-10-21 11:00:04 +0530

    Update linkedinj.py

--- linkedinj.py
+++ linkedinj.py
@@ -5,6 +5,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import os
 import time
@@ -13,14 +15,8 @@ import streamlit
 import spacy
 
 
-CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
-    
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install())
 
 url = 'https://www.linkedin.com/jobs/search?keywords=Data%20Scientist&location=India'
 #driver = webdriver.Chrome(r"C:\Users\User\Desktop\PROGRAM_FILES\chromedriver.exe")
