4926f465a23fcadc5b3de87aed181a7c094b40cb
Ayan <59755186+ayanatherate@users.noreply.github.com>
2023-04-22 01:03:34 +0530

    spacy default location added

--- extract_skills_sup.py
+++ extract_skills_sup.py
@@ -3,8 +3,6 @@ import pandas as pd
 import spacy
 import warnings
 warnings.filterwarnings('ignore')
-#nlp = spacy.load(r"https://github.com/ayanatherate/findmyjobbot/main/files/en_core_web_sm/en_core_web_sm-3.2.0")
-
 
 
 def extract_skills(resume_text:str) -> list[str]:
@@ -13,8 +11,8 @@ def extract_skills(resume_text:str) -> list[str]:
     function to extract skills from job descriptions,
     after matching with pre-loaded corpus of all possible
     skills
-    """
-    nlp = spacy.load(r"files/en_core_web_sm/en_core_web_sm_3-2-0") 
+    """ 
+    nlp = spacy.load("en_core_web_sm-3.2.0")
     nlp_text = nlp(resume_text)
     tokens = [token.text for token in nlp_text if not token.is_stop]
     data = pd.read_csv(r"https://raw.githubusercontent.com/ayanatherate/findmyjobbot/main/files/skills_corp.csv.csv") 
